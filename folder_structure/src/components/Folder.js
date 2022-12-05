095c752175e458942f523d5ba47dfe172f11fdc0
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-12-05 22:28:13 +0530

    added onClick

--- folder_structure/src/components/Folder.js
+++ folder_structure/src/components/Folder.js
@@ -1,13 +1,19 @@
+import {useState} from "react";
 
 function Folder({explorer}){
   console.log(explorer)
+
+const [expand, setExpand] = useState(false);
+
   if(explorer.isFolder){
 return(
     <div style = {{marginTop : 5}}>
-        <div>
+
+        <div className= "folder" onClick = {()=> setExpand(!expand)}>
             <span> 📁{explorer.name}</span>
         </div>
-        <div>
+
+        <div style = {{display:expand ? "block" : "none"}}>
             {explorer.items.map((exp)=>{
                 return(
                     <span>
@@ -20,7 +26,7 @@ return(
     </div>
 )
         }else{
-    return <span>📃{explorer.name}</span>
+    return <span className="file">📃{explorer.name}</span>
 }
     
 
