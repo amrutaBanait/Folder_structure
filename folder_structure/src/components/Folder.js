13b92e6322be0e9b98250282f92152e6bd60a5c9
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-12-09 18:04:23 +0530

    modified

--- folder_structure/src/components/Folder.js
+++ folder_structure/src/components/Folder.js
@@ -1,75 +1,74 @@
-import {useState} from "react";
+import { useState } from "react";
 
-function Folder({ handleInsertNode = () => {} , explorer}){
-  //console.log(explorer)
+function Folder({ handleInsertNode = () => {}, explorer }) {
+  const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: false
+  });
 
-const [expand, setExpand] = useState(false);
-const [showInput, setShowInput] = useState({
-    visible:false,
-    isFolder:null
-});
-
-//To stop event propagation
-const handleNewFolder = (e, isFolder) => {
+  const handleNewFolder = (e, isFolder) => {
     e.stopPropagation();
     setExpand(true);
-    setShowInput ({
-        visible:true,
-        isFolder
+    setShowInput({
+      visible: true,
+      isFolder
     });
-};
+  };
 
-//function to add  new folder
-const newFolder = (e) => {
-if(e.keyCode === 13 && e.target.value){
-  handleInsertNode(explorer.id. e.target.value,showInput.isFolder);
-    setShowInput({ ...showInput , visible: false })
-};
-}
-  if(explorer.isFolder){
-return (
-  <div style={{ marginTop: 5 }}>
-    <div className="folder" onClick={() => setExpand(!expand)}>
-      <span> 📁{explorer.name}</span>
+  const onAddFolder = (e) => {
+    if (e.keyCode === 13 && e.target.value) {
+      handleInsertNode(explorer.id, e.target.value, showInput.isFolder);
 
-      <div>
-        <button onClick={(e) => handleNewFolder(e, true)}> Folder +</button>
-        <button onClick={(e) => handleNewFolder(e, false)}>File +</button>
-      </div>
-    </div>
+      setShowInput({ ...showInput, visible: false });
+    }
+  };
 
-    <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
-      {showInput.visible && (
-        <div className="inputContainer">
-          <span>{showInput.isFolder ? "📒" : "📃"}</span>
-          <input 
-          type="text"
-          onKeyDown={newFolder}
-          onBlur = {() => setShowInput({...showInput, visible:false})}
-          className="inputContainer__input" 
-          autoFocus
-         
-          />
+  if (explorer.isFolder) {
+    return (
+      <div style={{ marginTop: 5 }}>
+        <div onClick={() => setExpand(!expand)} className="folder">
+          <span>📁 {explorer.name}</span>
+
+          <div>
+            <button onClick={(e) => handleNewFolder(e, true)}>Folder +</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>File +</button>
+          </div>
         </div>
-      )}
-      {explorer.items.map((exp) => {
-        return (
-        <Folder 
-        handleInsertNode={handleInsertNode}
-        explorer={exp} 
-        key={exp.name}
-         />
-      )
-      })}
-    </div>
-  </div>
-)
-        }else{
-    return <span className="file">📃{explorer.name}</span>
-}
+
+        <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
+          {showInput.visible && (
+            <div className="inputContainer">
+           <span>{showInput.isFolder? "📁" : "📄"}</span> 
+           <input
+              type="text"
+              className="inputContainer__input"
+              autoFocus
+              onKeyDown={onAddFolder}
+              onBlur={() => setShowInput({ ...showInput, visible: false })}
+              />
+              </div>
+          )}
+
+          {explorer.items.map((exp) => {
+            return (
+              <Folder
+                handleInsertNode={handleInsertNode}
+                key={exp.name}
+                explorer={exp}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return <span className="file">📄 {explorer.name}</span>;
+  }
 }
 
 export default Folder;
+
 
 
 
--- folder_structure/src/hooks/use-traverse-tree.js
+++ folder_structure/src/hooks/use-traverse-tree.js
@@ -22,44 +22,12 @@ const useTraverseTree = () => {
 
   };
 
-  const deleteNode = () => {};
 
-  const renameNode = () => {};
 
-  return { insertNode, deleteNode, renameNode};
+  return  insertNode;
 };
 
 
 export default useTraverseTree;
 
 
-// const useTraverseTree = () => {
-//   // Add a file or folder in tree
-//   // Can be optimised using Dynamic Programming
-//   const insertNode = function (tree, folderId, item, isFolder) {
-//     if (tree.id === folderId && tree.isFolder) {
-//       tree.items.unshift({
-//         name: item,
-//         isFolder: isFolder,
-//         items: []
-//       });
-
-//       return tree;
-//     }
-
-//     let latestNode = [];
-//     latestNode = tree.items.map((ob) => {
-//       return insertNode(ob, folderId, item, isFolder);
-//     });
-
-//     return { ...tree, items: latestNode };
-//   };
-
-//   const deleteNode = () => {}; // Do it Yourself
-
-//   const renameNode = () => {}; // Do it Yourself
-
-//   return { insertNode, deleteNode, renameNode };
-// };
-
-// export default useTraverseTree;
