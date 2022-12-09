03512b1f2b019c34cacd4b7f2c905e229c64b594
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-12-09 17:22:13 +0530

    insertNode function updates

--- folder_structure/src/hooks/use-traverse-tree.js
+++ folder_structure/src/hooks/use-traverse-tree.js
@@ -1,10 +1,12 @@
 const useTraverseTree = () => {
-  function insertNode(tree, folderId, item, isFolder) {
-    if ( tree.id === folderId && isFolder ) {
-      tree.item.unshift({
-        id: new Date().getTime(),
+
+  //to add file or folder in a tree
+  const insertNode = function (tree, folderId, item, isFolder) {
+    if ( tree.id === folderId && tree.isFolder ) {
+      tree.items.unshift({
+      //  id: new Date().getTime(),
         name: item,
-        isFolder,
+        isFolder: isFolder,
         items: [],
       });
     }
@@ -14,3 +16,35 @@ const useTraverseTree = () => {
 };
 
 export default useTraverseTree;
+
+
+// const useTraverseTree = () => {
+//   // Add a file or folder in tree
+//   // Can be optimised using Dynamic Programming
+//   const insertNode = function (tree, folderId, item, isFolder) {
+//     if (tree.id === folderId && tree.isFolder) {
+//       tree.items.unshift({
+//         name: item,
+//         isFolder: isFolder,
+//         items: []
+//       });
+
+//       return tree;
+//     }
+
+//     let latestNode = [];
+//     latestNode = tree.items.map((ob) => {
+//       return insertNode(ob, folderId, item, isFolder);
+//     });
+
+//     return { ...tree, items: latestNode };
+//   };
+
+//   const deleteNode = () => {}; // Do it Yourself
+
+//   const renameNode = () => {}; // Do it Yourself
+
+//   return { insertNode, deleteNode, renameNode };
+// };
+
+// export default useTraverseTree;
